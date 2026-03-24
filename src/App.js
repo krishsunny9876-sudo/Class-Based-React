54ddd62e834575e98530d3a6d80bf43aecb59b44
Kirtan <krishsunny9876@gmail.com>
2026-03-24 19:04:53 +0530

    app.js #2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  c = "Kirten";
   render() {
     return (
       <div>
-        
+        Hello my first class components {this.c}
       </div>
     )
   }
