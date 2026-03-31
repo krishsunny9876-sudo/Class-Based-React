d0099ec5f32419d0a3b9cb7177e9d8e29489eb0a
Kirtan <krishsunny9876@gmail.com>
2026-03-31 17:11:34 +0530

    march 31 and 2026 ' s first commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,13 +5,19 @@ export class News extends Component {
   render() {
     return (
       <>
-        <div className='container'>
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
+        <div className='container my-3'>
+          <h2>News Bobsun-Top Headlines</h2>
+          <div className='row'>
+            <div className='col-md-4'>
+              <NewsItem title="my title" description="my description" imgurl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" />
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="my title" description="my description" />
+            </div>
+            <div className='col-md-4'>
+              <NewsItem title="my title" description="my description" />
+            </div>
+          </div>
         </div>
       </>
     )
