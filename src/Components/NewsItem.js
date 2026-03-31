9235812ced1a26a31fc63ede33987e2be6b6a1c9
Kirtan <krishsunny9876@gmail.com>
2026-03-31 18:51:59 +0530

    make cards according to fetching data

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imgurl } = this.props;
+        let { title, description, imgUrl, newsUrl } = this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imgurl} className="card-img-top" alt="..." />
+                    <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
