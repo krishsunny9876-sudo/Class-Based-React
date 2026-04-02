e5237d19335eeb00547a14bfcd0b676d0b204af1
Kirtan <krishsunny9876@gmail.com>
2026-04-02 18:57:48 +0530

    02-04-2026 first save

--- src/Components/News.js
+++ src/Components/News.js
@@ -34,9 +34,21 @@ export class News extends Component {
     super();
     this.state = {
       articles: this.articles,
-      loading: false
+      loading: false,
+      page: 1
     }
   }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=081588361a054a0f930185b82927f00c"
+    let data = await fetch(url);
+    let data_object = await data.json();
+
+    console.log(data_object);
+
+    this.setState({ articles: data_object.articles })
+  }
+
   render() {
     return (
       <>
@@ -47,12 +59,18 @@ export class News extends Component {
             {this.state.articles.map((element) => {
               return (
                 <div className='col-md-4' key={element.url}>
-                  <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imgUrl={element.urlToImage}
+                  <NewsItem title={element.title ? element.title.slice(0, 45) : ""}
+                    description={element.description ? element.description.slice(0, 88) : ""}
+                    imgUrl={element.urlToImage ? element.urlToImage : "https://cdn.pixabay.com/photo/2016/11/21/06/53/beautiful-natural-image-1844362_1280.jpg"}
                     newsUrl={element.url} />
                 </div>
               )
             })}
+          </div>
 
+          <div className="container">
+            <button type="button" class="btn btn-dark">Prev</button>
+            <button type="button" class="btn btn-dark">Next</button>
           </div>
         </div>
       </>
