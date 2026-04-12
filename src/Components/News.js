44750b9e16360004003963fef8ddd37011b13ab4
Kirtan <krishsunny9876@gmail.com>
2026-04-12 11:59:13 +0530

    just complete infinte scroll system

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Loading from './loading'
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
+
+  capitalize(value) {
+    return (value[0].toUpperCase() + value.replace(value[0], ""));
+  }
 
   static defaultProps = {
     pageSize: 15,
@@ -18,7 +23,6 @@ export class News extends Component {
   };
 
   async updateNews() {
-    this.setState({ loading: true })
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=ad38da3716dc44eda8ba970297cb6409&page=${this.state.page}&pageSize=${this.props.pageSize}`
     let data = await fetch(url);
     let data_object = await data.json();
@@ -30,28 +34,29 @@ export class News extends Component {
     })
   }
 
-  handleNextClick = async () => {
-    this.setState({
-      page: this.state.page + 1
-    })
-    this.updateNews();
-  }
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 })
 
-  handlePrevClick = async () => {
-    this.setState({
-      page: this.state.page - 1
-    })
-    this.updateNews();
-  }
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=ad38da3716dc44eda8ba970297cb6409&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    let data = await fetch(url);
+    let data_object = await data.json();
 
-  constructor() {
-    super();
+    this.setState({
+      articles: this.state.articles.concat(data_object.articles),
+      totalResults: data_object.totalResults,
+      loading: false
+    })
+  };
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
       totalResults: 0
     }
+    document.title = `${this.capitalize(this.props.category)}-NewsBobsun`;
   }
 
   async componentDidMount() {
@@ -62,27 +67,32 @@ export class News extends Component {
     return (
       <>
         <div className='container my-3'>
-          <h1 className='text-center'>News Bobsun-Top Headlines</h1>
+          <h1 className='text-center'>NewsBobsun-Top Headlines on {this.capitalize(this.props.category)}</h1>
 
-          {this.state.loading && <Loading />}
+          {this.state.loading && <Loading over={true} />}
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length !== this.state.totalResults}
+            loader={<Loading over={this.state.articles.length < this.state.totalResults} />}
+          >
+            <div className="container">
+              {<div className='row'>
+                {this.state.articles.map((element) => {
+                  return (
+                    <div className='col-md-4' key={element.url}>
+                      <NewsItem title={element.title ? element.title.slice(0, 45) : ""}
+                        description={element.description ? element.description.slice(0, 88) : ""}
+                        imgUrl={element.urlToImage ? element.urlToImage : "https://cdn.pixabay.com/photo/2016/11/21/06/53/beautiful-natural-image-1844362_1280.jpg"}
+                        newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                    </div>
+                  )
+                })}
+              </div>}
+            </div>
 
-          {!this.state.loading && <div className='row'>
-            {this.state.articles.map((element) => {
-              return (
-                <div className='col-md-4' key={element.url}>
-                  <NewsItem title={element.title ? element.title.slice(0, 45) : ""}
-                    description={element.description ? element.description.slice(0, 88) : ""}
-                    imgUrl={element.urlToImage ? element.urlToImage : "https://cdn.pixabay.com/photo/2016/11/21/06/53/beautiful-natural-image-1844362_1280.jpg"}
-                    newsUrl={element.url} author={element.author} date={element.publishedAt} />
-                </div>
-              )
-            })}
-          </div>}
+          </InfiniteScroll>
 
-          <div className="container d-flex justify-content-between">
-            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
-            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-          </div>
         </div>
       </>
     )
--- src/Components/loading.js
+++ src/Components/loading.js
@@ -4,7 +4,7 @@ import loadings from './loadingss.gif'
 export default class loading extends Component {
     render() {
         return (
-            <div className='text-center my-3'>
+            this.props.over && <div className='text-center my-3'>
                 <img src={loadings} alt="loading" style={{ width: '50px' }} />
             </div>
         )
